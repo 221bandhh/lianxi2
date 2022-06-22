9cb6e776aaed8843a4c0a49540d9a67c132645fd
221bandhh <107937395+221bandhh@users.noreply.github.com>
2022-06-22 13:52:01 +0800

    Add files via upload

---
+++ 源1.cpp
@@ -0,0 +1,13 @@
+//输出1-100之间的奇数
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	int a = i++;
+	if (a <= 100 && a % == 0)
+		printf("%d\n是奇数"，a);
+	else(a <= 100 && a %= = 0)
+		printf("%d\n是偶数" ，a);
+	else(a>=100)
+	return 0;
+}
